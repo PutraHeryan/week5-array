c625dfbf7d8a7e46fc879da8f2ec8762f791d1be
PutraHeryan <149023563+PutraHeryan@users.noreply.github.com>
2025-09-19 20:48:36 +0800

    Add files via upload

--- arrayMethods_F1D022087.js
+++ arrayMethods_F1D022087.js
@@ -23,7 +23,7 @@ function hitungKataAyam() {
     }, 0);
 }
 
-function cariElemenDenganHurufS() {
+function carinamaDenganHurufS() {
     return daftarMakanan.find(makanan => makanan.includes('s'));
 }
 
@@ -42,10 +42,10 @@ function main() {
     });
 
     console.log("===== Pilih Metode Array yang Ingin Digunakan =====");
-    console.log("1. Ubah setiap elemen menjadi italic (map)");
+    console.log("1. Ubah setiap nama menjadi italic (map)");
     console.log("2. Filter nama yang mengandung 'goreng' (filter)");
     console.log("3. Hitung jumlah kata 'ayam' (reduce)");
-    console.log("4. Cari elemen pertama dengan huruf 's' (find)");
+    console.log("4. Cari nama pertama dengan huruf 's' (find)");
     console.log("5. Cek apakah ada nama < 6 huruf (some)");
     console.log("6. Cek apakah semua nama huruf kecil (every)");
     console.log("==================================================");
@@ -63,14 +63,14 @@ function main() {
                 hasil = `Jumlah kata 'ayam' dalam array: ${hitungKataAyam()}`;
                 break;
             case '4':
-                const ditemukan = cariElemenDenganHurufS();
-                hasil = `Elemen pertama yang ditemukan dengan huruf 's': ${ditemukan || 'Tidak ada'}`;
+                const ditemukan = carinamaDenganHurufS();
+                hasil = `nama pertama yang ditemukan dengan huruf 's': ${ditemukan || 'Tidak ada'}`;
                 break;
             case '5':
-                hasil = `Apakah ada elemen < 6 huruf? ${cekPanjangKurangDariEnam()}`;
+                hasil = `Apakah ada nama < 6 huruf? ${cekPanjangKurangDariEnam()}`;
                 break;
             case '6':
-                hasil = `Apakah semua elemen menggunakan huruf kecil? ${cekSemuaHurufKecil()}`;
+                hasil = `Apakah semua nama menggunakan huruf kecil? ${cekSemuaHurufKecil()}`;
                 break;
             default:
                 hasil = "Pilihan tidak valid. Silakan masukkan angka antara 1 sampai 6.";
